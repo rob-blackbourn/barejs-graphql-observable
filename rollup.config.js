783cf402b6d88c880c5777126d777897ab51ebb4
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-09-20 08:59:08 +0100

    Marked @barejs/graphql-client as external

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,41 @@
-import { terser } from "rollup-plugin-terser";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default [
   // browser-friendly UMD build
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
       {
-        name: "observableGraphqlClient",
+        name: 'observableGraphqlClient',
         file: 'dist/browser/index.js',
-        format: "umd",
+        format: 'umd'
       },
       {
-        name: "observableGraphqlClient",
+        name: 'observableGraphqlClient',
         file: 'dist/browser/index.min.js',
-        format: "umd",
+        format: 'umd',
         sourcemap: true,
-        plugins: [terser()],
-      },
+        plugins: [terser()]
+      }
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-    ],
+    plugins: [resolve(), commonjs()]
   },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	{
-		input: 'src/index.js',
-		external: ['graphqlClient', 'rxjs'],
-		output: [
-			{
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.js',
+    external: ['rxjs', '@barejs/graphql-client'],
+    output: [
+      {
         file: 'dist/index.js',
         format: 'es'
       },
-			{
+      {
         file: 'dist/cjs/index.js',
         format: 'cjs'
       }
-		]
-	}
-];
+    ]
+  }
+]
